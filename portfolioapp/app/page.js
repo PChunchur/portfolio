04c62f022a6fad2b9757231a744c3c94d44103a2
Pranav Chunchur <112144655+PChunchur@users.noreply.github.com>
2023-06-20 23:36:18 +0530

    closes #1 added alt to images

--- portfolioapp/app/page.js
+++ portfolioapp/app/page.js
@@ -11,17 +11,19 @@ export default function Home() {
       <header className='flex flex-row h-max w-full items-center justify-between bg-white px-3 lg:px-10'>
         <Image
           className='w-[70px] lg:w-[120px]'
-          src={'/logo-wobg.png'}
+          src={'/logo-wobg-min.png'}
           width={LOGO_SIZE}
           height={LOGO_SIZE}
+          alt={'PC\'s logo image'}
         ></Image>
         <aside className='flex flex-row gap-5 lg:gap-10 w-max h-max'>
           <a href='https://github.com/PChunchur' target={'_blank'}>
             <Image
               className='w-[30px] lg:w-[50px]'
-              src={'/github.png'}
+              src={'/github-min.png'}
               width={ICON_SIZE}
               height={ICON_SIZE}
+              alt={'github image'}
             ></Image>
           </a>
           <a href='https://www.linkedin.com/in/pranavchunchur/' target={'_blank'}>
@@ -30,29 +32,32 @@ export default function Home() {
               src={'/linkedin.png'}
               width={ICON_SIZE}
               height={ICON_SIZE}
+              alt={'linkedin icon'}
             ></Image>
           </a>
           <a href='mailto:chunchur.pranav@gmail.com' target={'_blank'}>
             <Image
               className='w-[30px] lg:w-[50px]'
-              src={'/email.png'}
+              src={'/email-min.png'}
               width={ICON_SIZE}
               height={ICON_SIZE}
+              alt={'picture of mail icon'}
             ></Image>
           </a>
         </aside>
       </header>
-      <section className='flex flex-col w-full h-full justify-start items-center bg-white gap-5'>
-        <article className='flex flex-col gap-5 items-center justify-center'>
+      <section className='flex flex-col w-full h-full justify-start items-center bg-white gap-10'>
+        <article className='flex flex-col gap-10 items-center justify-center'>
           <Image
             className='w-[150px] lg:w-[200px] rounded-md shadow-lg shadow-black'
             src={'/pranav.png'}
             width={PROFILE_PIC_SIZE}
             height={PROFILE_PIC_SIZE}
+            alt={'picture of pranav'}
           ></Image>
           <h1 className='font-bold text-2xl lg:text-4xl'>PRANAV</h1>
         </article>
-        <article className='flex w-10/12 text-center'>
+        <article className='flex w-8/12 text-center'>
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </article>
       </section>
